b0cd6b86baa982e2456c4423006d2ce1d881993e
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-05-04 06:19:25 -0700

    pagination with api call

--- src/App.js
+++ src/App.js
@@ -1,42 +1,92 @@
-import React,{useState,useEffect} from 'react';
-import axios from 'axios';
-import { Posts } from './components/Posts';
-import { Pagination } from './components/Pagination';
 import "./App.css";
 
+import ReactPaginate from "react-paginate";
+import { useEffect, useState } from "react";
 
-const App =()=> {
-  const [posts,setPosts]=useState([]);
-  const[loading,setLoading]=useState(false);
-  const[currentPage,setCurrentPage]=useState(1);
-  const[postsPerPage]=useState(10);
+function App() {
+  const [items, setItems] = useState([]);
 
-  useEffect(()=>{
-    const fetchPosts=async()=>{
-      setLoading(true);
-      const res=await axios.get('https://jsonplaceholder.typicode.com/posts');
-      setPosts(res.data);
-      setLoading(false);
-    }
-    fetchPosts();
-  },[]);
-  console.log(posts);
+  const [pageCount, setpageCount] = useState(0);
 
+  let limit = 10;
 
-  //Get current posts
-  const indexOfLastPost=currentPage*postsPerPage;
-  const indexOfFirstPost=indexOfLastPost-postsPerPage;
-  const currentPosts=posts.slice(indexOfFirstPost,indexOfLastPost);
+  useEffect(() => {
+    const getComments = async () => {
+      const res = await fetch(
+        `http://jsonplaceholder.typicode.com/comments?_page=1&_limit=${limit}`
+        // `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=${limit}`
+      );
+      const data = await res.json();
+      const total = res.headers.get("x-total-count");
+      setpageCount(Math.ceil(total / limit));
+      // console.log(Math.ceil(total/12));
+      setItems(data);
+    };
 
-  //Change page
-  const paginate=(pageNumber)=>setCurrentPage(pageNumber);  
+    getComments();
+  }, [limit]);
+
+  const fetchComments = async (currentPage) => {
+    const res = await fetch(
+      `http://jsonplaceholder.typicode.com/comments?${currentPage}&_limit=${limit}`
+      // `https://jsonplaceholder.typicode.com/comments?_page=${currentPage}&_limit=${limit}`
+    );
+    const data = await res.json();
+    return data;
+  };
+
+  const handlePageClick = async (data) => {
+    console.log(data.selected);
+
+    let currentPage = data.selected + 1;
+
+    const commentsFormServer = await fetchComments(currentPage);
+
+    setItems(commentsFormServer);
+    // scroll to the top
+    //window.scrollTo(0, 0)
+  };
   return (
-    <div className="container mt-5">
-      <h1 className='text-primary mb-3'>My Blog</h1>
-      <Posts posts={currentPosts} loading={loading}/>
-      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length}paginate={paginate}/>
+    <div className="container">
+      <div className="row m-2">
+        {items.map((item) => {
+          return (
+            <div key={item.id} className="col-sm-6 col-md-4 v my-2">
+              <div className="card shadow-sm w-100" style={{ minHeight: 225 }}>
+                <div className="card-body">
+                  <h5 className="card-title text-center h2">Id :{item.id} </h5>
+                  <h6 className="card-subtitle mb-2 text-muted text-center">
+                    {item.email}
+                  </h6>
+                  <p className="card-text">{item.body}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        breakClassName={"page-item"}
+        breakLinkClassName={"page-link"}
+        activeClassName={"active"}
+      />
     </div>
   );
-};
+}
 
 export default App;
