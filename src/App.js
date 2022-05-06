d19d37bbce4dbc84cc71fb715091c38da7485fe5
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-05-06 12:03:31 +0545

    paginationinreact

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
-import React from 'react';
-import { Toaster } from 'react-hot-toast';
-import AppContent from './components/AppContent';
-import AppHeader from './components/AppHeader';
-import PageTitle from './components/PageTitle';
-import styles from './styles/modules/app.module.scss';
+import React, { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
+import "./App.css";
 
+const PER_PAGE = 20;
 function App() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function fetchData() {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }
+  function handlePageClick({ selected: selectedPage }) {
+    console.log("selectedPage", selectedPage);
+    setCurrentPage(selectedPage);
+  }
+
+  //0,10,20,30....
+  const offset = currentPage * PER_PAGE;
+  //console.log("offset",offset)
+  const currentPageData = data
+    .slice(offset, offset + PER_PAGE)
+    .map((res, index) => <img key={index} src={res.thumbnailUrl} />);
+  //console.log("currentPageData",currentPageData)
+
+  //total pages:500
+  const pageCount = Math.ceil(data.length / PER_PAGE);
+
   return (
-    <>
-      <div className="container">
-        <PageTitle>TODO List</PageTitle>
-        <div className={styles.app__wrapper}>
-          <AppHeader />
-          <AppContent />
-        </div>
-      </div>
-      <Toaster
-        position="bottom-right"
-        toastOptions={{
-          style: {
-            fontSize: '1.4rem',
-          },
-        }}
+    <div className="App">
+      <h1>React Pagination</h1>
+      {currentPageData}
+      <ReactPaginate
+        previousLabel={"<-Previous"}
+        nextLabel={"Next->"}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        previousLinkClassName={"pagination__link"}
+        nextLinkClassName={"pagination__link"}
+        disabledClassName={"pagination__link--disabled"}
+        activeClassName={"pagination__link--active"}
       />
-    </>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import '@fontsource/poppins';
-import '@fontsource/poppins/500.css';
-import '@fontsource/poppins/600.css';
-import '@fontsource/poppins/700.css';
-import './styles/GlobalStyles.css';
-import { store } from './app/store';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
