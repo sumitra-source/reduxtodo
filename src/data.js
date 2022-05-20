b1a45555e308f100c52eb93060f3e14934bacf34
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-05-20 15:34:35 +0545

    frontend

--- src/data.js
+++ src/data.js
@@ -47,7 +47,7 @@ export const popularProducts = [
   },
   {
     id: 2,
-    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    img: "https://i.ibb.co/DG69bQ4/2.png",
   },
   {
     id: 3,
@@ -67,7 +67,7 @@ export const popularProducts = [
   },
   {
     id: 7,
-    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    img: "https://i.ibb.co/cXFnLLV/3.png",
   },
   {
     id: 8,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
+import Products from "../components/Products";
+import NewsLetter from "../components/NewsLetter";
+import Footer from "../components/Footer";
 const Home = () => {
   return (
     <div>
@@ -10,6 +13,9 @@ const Home = () => {
       <Navbar />
       <Slider />
       <Categories />
+      <Products />
+      <NewsLetter />
+      <Footer />
     </div>
   );
 };
